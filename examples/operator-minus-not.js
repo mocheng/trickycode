9f83509b52c908690f58a9baada669ef71483cfa
Morgan Cheng <morgan.chengmo@gmail.com>
2014-07-28 17:27:29 +0800

    add operator minius not

---
+++ examples/operator-minus-not.js
@@ -0,0 +1,9 @@
+/**
+ * integer value on combo operator '-~' result in value+1.
+ */
+console.log(-~undefined);
+console.log(-~0);
+console.log(-~1);
+console.log(-~2);
+console.log(-~-1);
+console.log(-~-2);
