f45d1c39ce63ac489fef60bb4633ed12523efdd0
Morgan Cheng <morgan.chengmo@gmail.com>
2014-07-29 09:00:56 +0800

    add NaN and null

--- examples/operator-minus-not.js
+++ examples/operator-minus-not.js
@@ -1,6 +1,9 @@
 /**
  * integer value on combo operator '-~' result in value+1.
+ * undefined, null, NaA are all treated as zero.
  */
+console.log(-~NaN);
+console.log(-~null);
 console.log(-~undefined);
 console.log(-~0);
 console.log(-~1);
